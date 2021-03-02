e13eec0cc651759e1c2e8f9f02b97ef63204bf03
Fatema-Nawrin <77042674+Fatema-Nawrin@users.noreply.github.com>
2021-03-03 00:46:29 +0600

    Add files via upload

---
+++ Fix_turnstile.py
@@ -0,0 +1,44 @@
+
+
+import csv
+
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    As you can see, there are numerous data points included in each row of the
+    a MTA Subway turnstile text file. 
+
+    You want to write a function that will update each row in the text
+    file so there is only one entry per row. A few examples below:
+    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
+    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
+    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    Write the updates to a different text file in the format of "updated_" + filename.
+    For example:
+        1) if you read in a text file called "turnstile_110521.txt"
+        2) you should write the updated data to "updated_turnstile_110521.txt"
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file by downloading these files from the resources:
+    
+    Sample input file: turnstile_110528.txt
+    Sample updated file: solution_turnstile_110528.txt
+    '''
+    
+    for name in filenames:
+        u="updated_"+name
+        reader = csv.reader(open(name,'r'))
+        writer = csv.writer(open(u,'w'))
+        for row in reader:
+            for x in range(3,len(row),5):
+                new_row =row[0:3]+row[x:x+5]
+                writer.writerow(new_row)
+fix_turnstile_data(['turnstile-110528.txt'])
+
+
+
+
+
+
+
